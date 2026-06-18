c9c0285a09dc9e10c65d410258949851585ed6ca
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-18 14:40:12 +0300

    mission details added

---
+++ mission_control.py
@@ -0,0 +1,11 @@
+name="shalom"
+mission_code=315436
+distens_to_target=2
+mission_active_status=True
+print(f"the agent name is, {name}, the mission code is {mission_code}")
+print("distens to target is", distens_to_target)
+print("the active status is", mission_active_status)
+print (type(name))
+print (type(mission_code))
+print (type(distens_to_target))
+print (type (mission_active_status))
