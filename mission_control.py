b86ef6b4bd7e77daf5a84a7894e5bde45cef0fbc
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-18 14:57:42 +0300

    more clear details

--- mission_control.py
+++ mission_control.py
@@ -1,14 +1,13 @@
-name="shalom"
+agent_name="shalom"
 mission_code=315436
-distens_to_target=2
+distens_to_target_in_km=2
 mission_active_status=True
-print(f"the agent name is, {name}, the mission code is {mission_code}")
-print("distens to target is", distens_to_target)
+print(f"the agent name is, {agent_name}, the mission code is {mission_code}")
+print("distens to target in km is", (distens_to_target_in_km))
 print("the active status is", mission_active_status)
-print (type(name))
+print (type(agent_name))
 print (type(mission_code))
-print (type(distens_to_target))
+print (type(distens_to_target_in_km))
 print (type (mission_active_status))
-travel_distance=20
-print ("full distance for a trip from base to target and back is", travel_distance)
- 
+travel_distance_in_km=20
+print ("full distance for a trip from base to target and back in km is", travel_distance_in_km)
