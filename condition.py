75c23d0a996e8655dae612b6dfc2452fea5cde28
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-21 13:26:35 +0300

    added temperature

---
+++ condition.py
@@ -0,0 +1,11 @@
+given_age=int(input("what is your age? "))
+if given_age > 17:
+    print("can enter")
+else:
+    print("Cannot enter")
+temperature = 38.2
+if temperature > 37.5:
+    print("High temperature")
+else:
+    print("Normal temperature")
+    
