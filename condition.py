9c7bb9b0366d609a590915660385f18dcf31491b
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-21 14:04:19 +0300

    battery added

--- condition.py
+++ condition.py
@@ -15,5 +15,15 @@ given_number=int(input("give a number "))
 if given_number % 2 == 0:
     print("Even number")
 else:
-    print("Odd number")    
+    print("Odd number")  
+#section 4
+battery = 15 
+is_charging = True
+if battery < 20 and is_charging == True:
+    print ("Low battery, charging now")
+elif battery < 20 and is_charging == False:
+    print("Low battery, connect charger")   
+else:
+    print("Battery OK") 
+           
 
