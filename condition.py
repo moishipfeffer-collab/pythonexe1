b6da153ad24a666be90f7496071df1697d2fe4e7
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-21 13:58:22 +0300

    number added

--- condition.py
+++ condition.py
@@ -1,11 +1,19 @@
+#section 1
 given_age=int(input("what is your age? "))
 if given_age > 17:
     print("can enter")
 else:
     print("Cannot enter")
+#section 2
 temperature = 38.2
 if temperature > 37.5:
     print("High temperature")
 else:
     print("Normal temperature")
-    
+#section 3
+given_number=int(input("give a number "))
+if given_number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")    
+
