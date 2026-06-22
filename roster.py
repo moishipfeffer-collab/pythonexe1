8f0ce94ced873dbe3e10c9ba3309908339abec90
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-22 14:15:27 +0300

    agent list added

---
+++ roster.py
@@ -0,0 +1,13 @@
+agents=["Alpha", "Bravo", "Charlie", "Delta", "Echo"]
+# section 1
+print(agents)
+#section 2
+print(agents[0:5:4])
+#section 3
+print(agents[1])
+#section 4
+print(agents[1:4])
+#section 5
+agents.append("Foxtrot")
+print(agents)
+
